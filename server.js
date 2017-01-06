d83da33a619dc8926df75ecbd804fffafc6ed669
Kelvin Mbewe <keokeil@hotmail.com>
2017-01-06 14:57:07 +0100

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express = require('express');
+var express = require('express');//we're using the express server thats why its being loaded first.
 
-// Create our app
+
 var app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000; //heroku will give us the port to use through the process.env.PORT or else using the default PORT 3000
 
 app.use(function(req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
-
+//the above code forwards all https addresses to http
   } else {
-    next();
+    next(); //if the address is already http then the address just goes through
   }
 });
 
@@ -17,5 +18,5 @@ app.use(function(req, res, next) {
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
+  console.log('Express server is up on port ' + PORT);//heroku will tell us which localhost server to use
 });
