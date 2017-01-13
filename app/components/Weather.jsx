b26b41c6612d8a1169d5cc7646f426be442154e1
Kelvin Mbewe <keokeil@hotmail.com>
2017-01-13 12:35:44 +0100

    add nav search support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -28,7 +28,9 @@ handleSearch: function(location) {
 
   this.setState({
     isLoading: true,
-    errorMessage: undefined
+    errorMessage: undefined,
+    location: undefined,
+    temp: undefined
   });
 
   openWeatherMap.getTemp(location).then(function(objData) {
@@ -41,10 +43,25 @@ handleSearch: function(location) {
 }, function (e) {
     that.setState({
       isLoading: false,
-
+      errorMessage: e.message
     });
 });
   },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
     function renderMessage() {
